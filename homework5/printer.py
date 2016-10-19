1850776168c2cb660dced4c346fae26ebfbc655f
dkaznacheev <dmitry.kaznacheev@gmail.com>
2016-10-19 21:55:04 +0300

    removed duplication

--- homework5/printer.py
+++ homework5/printer.py
@@ -5,121 +5,74 @@ from yat.model import Number, FunctionCall, Reference, FunctionDefinition, \
 class PrettyPrinter:
     def __init__(self):
         self.indent = 0
-        self.is_sentence = True
 
     def visit(self, expr):
         expr.visit(self)
+        print(";")
 
-    def visitNumber(self, number):
-        if self.is_sentence:
-            print("\t" * self.indent, end="")
+    def visitNumber(self, number):            
         print(number.value, end="")
-        if self.is_sentence:
-            print(";")
-
-    def visitReference(self, reference):
-        if self.is_sentence:
-            print("\t" * self.indent, end="")
-        print(reference.name, end="")
-        if self.is_sentence:
-            print(";")
+    
+    def visitReference(self, reference):     
+        print(reference.name, end="")  
 
     def visitFunctionCall(self, fcall):
-        if self.is_sentence:
-            print("\t" * self.indent, end="")
-        prev_state = self.is_sentence
-        self.is_sentence = False
         fcall.fun_expr.visit(self)
         print("(", end="")
         for i in range(len(fcall.args)):
             fcall.args[i].visit(self)
             if i != len(fcall.args) - 1:
                 print(', ', end = "")
-        print(")", end="")
-        self.is_sentence = prev_state
-        if self.is_sentence:
-            print(";")
+        print(")", end="")          
 
-    def visitFunctionDefinition(self, fdef):
-        if self.is_sentence:
-            print("\t" * self.indent, end="")
+    def visitBody(self, body):
+        self.indent += 1
+        for sentence in body:
+            print("\t" * self.indent, end = "")
+            sentence.visit(self)
+            print(';')
+        self.indent -= 1
+        
+    def visitFunctionDefinition(self, fdef): 
         print("def " + fdef.name + "(", end="")
         print(", ".join(fdef.function.args) + ") {")
-        self.indent += 1
-        for sentence in fdef.function.body:
-            sentence.visit(self)
-        self.indent -= 1
+        self.visitBody(fdef.function.body)
         print("\t" * self.indent + "}", end="")
-        if self.is_sentence:
-            print(";")
 
     def visitUnaryOperation(self, unop):
-        if self.is_sentence:
-            print("\t" * self.indent, end="")
         print("(" + unop.op, end="")
-
-        prev_state = self.is_sentence
-        self.is_sentence = False
-        unop.expr.visit(self)
-        self.is_sentence = prev_state
-
+        unop.expr.visit(self)        
         print(")", end="")
 
-        if self.is_sentence:
-            print(";")
-
     def visitBinaryOperation(self, binop):
-        if self.is_sentence:
-            print("\t" * self.indent, end="")
-
-        prev_state = self.is_sentence
-        self.is_sentence = False
         print("(", end="")
         binop.lhs.visit(self)
         print(") " + binop.op + " (", end="")
         binop.rhs.visit(self)
-        print(")", end="")
-        self.is_sentence = prev_state
-
-        if self.is_sentence:
-            print(";")
+        print(")", end="")       
 
     def visitPrint(self, printer):
-        print("\t" * self.indent + "print ", end="")
-
-        self.is_sentence = False
-        printer.expr.visit(self)
-        self.is_sentence = True
-
-        print(";")
+        print("print ", end = "")
+        printer.expr.visit(self)  
 
     def visitRead(self, reader):
-        print("\t" * self.indent + "read " + reader.name + ";")
-
-    def visitBranch(self, branch):
-        for sentence in branch:
-            sentence.visit(self)
+        print("read " + reader.name, end = "")
         
     def visitConditional(self, cond):
-        print("\t" * self.indent + "if (", end="")
-        self.is_sentence = False
+        print("if (", end="")
         cond.condition.visit(self)
-        self.is_sentence = True
-
-        print(") {")
-        self.indent += 1
+        print(") {")    
         if cond.if_true:
-            self.visitBranch(cond.if_true)
+            self.visitBody(cond.if_true)
         if cond.if_false:
-            print("\t" * (self.indent - 1) + "} else {")
-            self.visitBranch(cond.if_false)
-        self.indent -= 1
-        print("\t" * self.indent + "};")
+            print("\t" * (self.indent) + "} else {")
+            self.visitBody(cond.if_false)
+        print("\t" * self.indent + "}", end = "")
 
 def test():
     f1 = Conditional(FunctionCall(Reference('x'), [Number(22)]), [Conditional(BinaryOperation(Number(0), '-', Number(6)), [],[Conditional(Number(0), [Conditional(UnaryOperation('-', Number(20)), [],[FunctionDefinition('foobar', Function(['ab', 'cd'], [
     Print(BinaryOperation(UnaryOperation('-', Number(120)), '*', BinaryOperation(UnaryOperation('-', Number(20)), '+', Reference('z')))), Read('x')
-    ]))])],[])])],[Conditional(Number(0), [Conditional(Number(0), [],[])],[])])
+    ]))])],[])])],[Conditional(Number(0), [Conditional(Number(0), [],[Read('xxx')])],[])])
 
     f = FunctionDefinition('foo', Function(['a', 'b'], [
     FunctionDefinition('bar', Function(['c', 'd'], [
