9b338d69e0aa432f06600cd60b7a5b82e5f3440e
Dmitry Kaznacheev <dmitry.kaznacheev@gmail.com>
2016-11-20 20:40:45 +0300

    Added whitespaces

--- homework8/Fp1.hs
+++ homework8/Fp1.hs
@@ -1,17 +1,24 @@
 head' (x:xs) = x
+
 tail' [] = []
 tail' (x:xs) = xs
+
 take' 0 xs = []
 take' n (x:xs) = x:(take' (n - 1) xs)
+
 drop' 0 xs = xs
 drop' n [] = []
 drop' n (x:xs) = drop' (n - 1) xs
+
 filter' f [] = []
 filter' f (x:xs) | f x = x:(filter' f xs) | otherwise = filter' f xs
+
 foldl' (f) n [] = n
 foldl' (f) n xs = foldl' (f) (f n (head' xs)) (tail' xs)
+
 concat' [] ys = ys
 concat' xs ys = (head' xs):(concat' (tail' xs) ys)
+
 quickSort' [] = []
 quickSort' [x] = [x]
 quickSort' xs =  (quickSort' (filter (< pivot xs) xs)) 
