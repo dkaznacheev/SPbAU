3674499e05cae634d7212ea705229454362a263b
Dmitry Kaznacheev <dmitry.kaznacheev@gmail.com>
2016-11-22 23:40:32 +0300

    quickSort' and bracket fix

--- homework8/Fp1.hs
+++ homework8/Fp1.hs
@@ -14,18 +14,18 @@ filter' f [] = []
 filter' f xs | f (head' xs) = (head' xs):filter' f (tail' xs) | otherwise = filter' f (tail' xs)
 
 foldl' (f) n [] = n
-foldl' (f) n xs = foldl' (f) (f n (head' xs)) (tail' xs)
+foldl' (f) n xs = foldl' (f) next_n (tail' xs) where
+    next_n = (f) n (head' xs)
 
 concat' [] ys = ys
 concat' xs ys = (head' xs):(concat' (tail' xs) ys)
 
 quickSort' [] = []
 quickSort' [x] = [x]
-quickSort' xs =  quickSort' lesser `concat'` equal `concat'` greater where
-    pivot = head' xs
+quickSort' xs =  quickSort' lesser `concat'` equal `concat'` quickSort' greater where
     lesser = filter (< pivot) xs
     equal = filter (== pivot) xs
     greater = filter (> pivot) xs
-    
+    pivot = head' xs
     
     
