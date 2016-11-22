40d708880bc01fd9816605eacd620fc1234b2823
Dmitry Kaznacheev <dmitry.kaznacheev@gmail.com>
2016-11-22 21:31:49 +0300

    Cosmetic fixes

--- homework8/Fp1.hs
+++ homework8/Fp1.hs
@@ -4,14 +4,14 @@ tail' [] = []
 tail' (x:xs) = xs
 
 take' 0 xs = []
-take' n (x:xs) = x:(take' (n - 1) xs)
+take' n xs = head' xs : take' (n - 1) (tail' xs)
 
 drop' 0 xs = xs
 drop' n [] = []
-drop' n (x:xs) = drop' (n - 1) xs
+drop' n xs = drop' (n - 1) (tail' xs)
 
 filter' f [] = []
-filter' f (x:xs) | f x = x:(filter' f xs) | otherwise = filter' f xs
+filter' f xs | f (head' xs) = (head' xs):filter' f (tail' xs) | otherwise = filter' f (tail' xs)
 
 foldl' (f) n [] = n
 foldl' (f) n xs = foldl' (f) (f n (head' xs)) (tail' xs)
@@ -21,11 +21,11 @@ concat' xs ys = (head' xs):(concat' (tail' xs) ys)
 
 quickSort' [] = []
 quickSort' [x] = [x]
-quickSort' xs =  (quickSort' (filter (< pivot xs) xs)) 
-                 `concat'` ((filter (== pivot xs) xs)) 
-                 `concat'` quickSort'((filter (> pivot xs) xs)) where
-    len [] = 0
-    len xs = 1 + len (tail' xs)
-    pivot xs = head' (drop' ((len xs) `div` 2) xs)
+quickSort' xs =  quickSort' lesser `concat'` equal `concat'` greater where
+    pivot = head' xs
+    lesser = filter (< pivot) xs
+    equal = filter (== pivot) xs
+    greater = filter (> pivot) xs
+    
     
     
